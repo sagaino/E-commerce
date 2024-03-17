0a1acca27122a7e28224cb9b9adfe06c26b5ef22
ino <sagaino18@gmail.com>
2024-03-17 23:40:35 +0700

    Remove unused code

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,10 +14,6 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
   else {
-    const userData: UserDataType = JSON.parse(`${cookie}`)
-    // if (userData.role?.name !== "superadmin" && request.nextUrl.pathname.startsWith('/admin')) {
-    //   return NextResponse.redirect(new URL('/dashboard', request.url))
-    // }
     if (request.nextUrl.pathname.startsWith('/login')) {
       return NextResponse.redirect(new URL('/', request.url))
     }
